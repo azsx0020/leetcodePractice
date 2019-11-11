24af4755be78162dce58603be47bae00ee124a7f
azsx0020 <adohong@163.com>
2019-11-12 07:50:20 +0800

    longest substring without repeating characters

---
+++ src/main/java/com/adohong/leetcode/practice/longestsubstringwithoutrepeatingcharacters/solution.java
@@ -0,0 +1,37 @@
+package com.adohong.leetcode.practice.longestsubstringwithoutrepeatingcharacters;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        int longestSize = 0;
+        Map<Character, Integer> longestSubstringCurrently = new LinkedHashMap<>();
+        for (char c : s.toCharArray()) {
+            if (null != longestSubstringCurrently.get(c)) {
+                Iterator<Entry<Character, Integer>> iter = longestSubstringCurrently.entrySet().iterator();
+                while (iter.hasNext()) {
+                    Map.Entry<Character, Integer> entry = iter.next();
+                    if (entry.getKey().equals(c)) {
+                        iter.remove();
+                        break;
+                    }
+                    iter.remove();
+                }
+            }
+            longestSubstringCurrently.put(c, 1);
+            if (longestSize < longestSubstringCurrently.size()) {
+                longestSize = longestSubstringCurrently.size();
+            }
+        }
+        return longestSize;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLongestSubstring("abcabcbb"));
+    }
+}
